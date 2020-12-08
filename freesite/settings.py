80c0d18d856ab337ea9ecae65d4099387c2408ae
pvs <vitaliypyhodko@gmail.com>
2020-12-08 19:07:45 +0300

    Set MEDIA_ROOT from BASE_DIR

--- freesite/settings.py
+++ freesite/settings.py
@@ -99,8 +99,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_ROOT = 'media/'
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 
