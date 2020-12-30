0929fe55b717a020fc662bf0317ee7448984511c
pvs <vitaliypyhodko@gmail.com>
2020-12-30 11:37:58 +0300

    Adding a license

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,31 @@
+"""
+Copyright (c) Vitaliy Prikhodko
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of Vitaliy Prikhodko nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL VITALIY PRIKHODKO BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+
 import os
 
 from django.shortcuts import render, get_object_or_404, redirect
@@ -14,9 +42,9 @@ logo_exists = False
 if os.path.isfile(os.path.join(STATIC_ROOT, 'logo.png')):
     logo_exists = True
 
-header_exists = False
+header_img_exists = False
 if os.path.isfile(os.path.join(STATIC_ROOT, 'header.png')):
-    header_exists = True
+    header_img_exists = True
 
 
 def menu_page(request, pk):
@@ -33,7 +61,7 @@ def menu_page(request, pk):
         'settings': settings[0],
         'favicon_exists': favicon_exists,
         'logo_exists': logo_exists,
-        'header_exists': header_exists
+        'header_img_exists': header_img_exists
     })
 
 
@@ -49,7 +77,7 @@ def page_list(request, pk):
         'settings': settings[0],
         'favicon_exists': favicon_exists,
         'logo_exists': logo_exists,
-        'header_exists': header_exists
+        'header_img_exists': header_img_exists
     })
 
 
@@ -63,7 +91,7 @@ def main_page(request):
         'settings': settings[0],
         'favicon_exists': favicon_exists,
         'logo_exists': logo_exists,
-        'header_exists': header_exists
+        'header_img_exists': header_img_exists
     })
 
 
@@ -77,6 +105,6 @@ def page(request, pk):
         'settings': settings[0],
         'favicon_exists': favicon_exists,
         'logo_exists': logo_exists,
-        'header_exists': header_exists
+        'header_img_exists': header_img_exists
     })
 
