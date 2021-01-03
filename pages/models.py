edb4e7da559e906f52092fa43dd63c08831acd9c
vitaliy-pvs <vitaliypryhodko@yandex.ua>
2021-01-03 16:36:56 +0300

    Replacing up_burger_color with up_button_color

--- pages/models.py
+++ pages/models.py
@@ -50,7 +50,7 @@ class MenuPage(models.Model):
 class Settings(models.Model):
     site_title = models.TextField(default="#")
     header_color = models.TextField(default="#")
-    up_burger_color = models.TextField(default="#")
+    up_button_color = models.TextField(default="#")
     menu_color = models.TextField(default="#")
     menu_text_color = models.TextField(default="#")
     page_color = models.TextField(default="#")
