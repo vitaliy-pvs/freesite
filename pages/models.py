aa4f5292203124c77d5cc25b11a970c2682bc4ca
vitaliy-pvs <vitaliypryhodko@yandex.ua>
2021-01-07 19:35:26 +0300

    Adding the output of the settings status report to the console

--- pages/models.py
+++ pages/models.py
@@ -49,11 +49,11 @@ class MenuPage(models.Model):
 
 class Settings(models.Model):
     site_title = models.TextField(default="#")
+    main_page_title = models.TextField(default="#")
+    page_list_title = models.TextField(default="#")
     header_color = models.TextField(default="#")
     up_button_color = models.TextField(default="#")
     menu_color = models.TextField(default="#")
     menu_text_color = models.TextField(default="#")
     page_color = models.TextField(default="#")
     page_text_color = models.TextField(default="#")
-    main_page_title = models.TextField(default="#")
-    page_list_title = models.TextField(default="#")
